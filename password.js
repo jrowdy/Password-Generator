69bb48f7ab925224ed79c0c2825f7afa9458303f
jrowdy <58674083+jrowdy@users.noreply.github.com>
2020-01-26 11:58:49 -0500

    Added more user options for password.

--- password.js
+++ password.js
@@ -1,51 +1,74 @@
-var password = '';
-
-alert("Welcome. This application will generate a password for you. Please answer the following questions.");
-
-var length = prompt("How many characters would you like? Choose a number between 8-128.")
-if(length <= 128 && length >= 8) {
-    console.log(length);
-} else {
-    alert("Remember, choose a number between 8-128."); 
-    prompt("How many characters would you like? Choose a number between 8-128.")
+var length = prompt("How many characters would you like? Choose a number between 8-128.");
+  if(length <= 128 && length >= 8) {
+      console.log(length);
+  } else {
+    alert("You've selected a number outside of the range. Please choose a number between 8-128.");
+    prompt("How many characters would you like? Choose a number between 8-128.");
+    if(length <= 128 && length >= 8) {
+        console.log(length);
 }
-// var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
-
-var charType = prompt("Choose a character type for your password: special, numeric, uppercase, lowercase.");
-
-
-function generatePassword() {
-  //evaluate character type
-  var password = "";
-  var charTypeLower = charType.toLowerCase();
-  if( charTypeLower === "lowercase" ) {
-    password = "abcdefghijklmnopqrstuvwxyz";
-  } else if( charTypeLower === "uppercase" ) {
-    password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if( charTypeLower === "numeric" ) {
-    password = "0123456789";
-  } else if( charTypeLower === "special" ) {
-    password = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  } 
-  //return value
-  var retVal = "";
-  for (var i = 0; i < length; i++) {
-    //picks a character within charSet at index of random number
-    retVal += password.charAt(Math.floor(Math.random() * password.length));
   }
+    
 
-  // add event listener for click event to print password
+var password = '';
+ var userLowercase = confirm("Would you like lowercase letters in your password?");
+ var userUppercase = confirm("Would you like uppercase letters in your password?");
+ var userNumeric = confirm("Would you like numbers in your password?");
+ var userSpecial = confirm("Would you like symbols in your password?");
+
+ if(userLowercase) {
+  var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  console.log("Lower, yes");
+    } else {
+  var lowercase = '';
+  console.log("Lower, no");
+}
+
+if(userUppercase) {
+  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  console.log("Upper, yes");
+    } else {
+  var uppercase = '';
+  console.log("Upper, no");
+}
+
+if(userNumeric) {
+  var numbers = '0123456789';
+  console.log("Numbers, yes");
+    } else {
+  var numbers = '';
+  console.log("Numbers, no");
+}
+
+if(userSpecial) {
+  var special = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  console.log("Special, yes");
+    } else {
+  var special = '';
+  console.log("Special, no");
+}
+
+
+var userPref = (lowercase+uppercase+numbers+special);
+console.log(userPref);
+
+if(userPref==="") alert("Please select OK, refresh the screen and select at least 1 choice.")
+
+var retVal= "";
+for(var i=0; i< length; i++) {
+    retVal += userPref.charAt(Math.floor(Math.random() * userPref.length));
+}
+
+// add event listener for click event to print password
 document.getElementById("generate").addEventListener("click", printPassword)
 
 // get element by ID to print password and add to console to validate
 function printPassword() {
-    document.getElementById("password").innerHTML = retVal;
+    document.getElementById("password").innerHTML = retVal; {
     console.log(retVal);
 };
   return retVal;
 }
-alert(generatePassword());
-
 // Copy password to clipboard
 document.getElementById("copy").addEventListener("click", copyPassword)
 
